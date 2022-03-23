a15a25b8a33c2bba5bfaeb92fe164ead34aad091
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-03-23 14:14:31 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const elText = document.querySelectorAll(".form-control");
+const elGrow = document.querySelectorAll(".range-grow");
+let count = 0;
+
+elText.forEach((item, index) => {
+  item.addEventListener("keyup", () => {
+    let value =  item.getAttribute('maxlength');
+    let n = 100/value;
+    if(index == 0){
+      elGrow[index].style.width = `${item.value.length * n}%`;
+      elGrow[index].textContent = `${item.value.length * n}%`;
+    } else if(index == 1){
+      elGrow[index].style.width = `${item.value.length}%`;
+      elGrow[index].textContent = `${item.value.length}%`;
+    } else if(index == 2){
+      elGrow[index].style.width = `${item.value.length / 10}%`;
+      elGrow[index].textContent = `${item.value.length / 10}%`;
+    }
+  });
+});
+
+
+
+
